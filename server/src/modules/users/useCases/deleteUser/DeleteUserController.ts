6b922b81b6de0fb59b13018c21c57a268338b9d2
Patrick Morais <ppternunes@gmail.com>
2023-04-22 16:00:10 -0300

    feat(DeleteUser): create Controller

---
+++ server/src/modules/users/useCases/deleteUser/DeleteUserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { DeleteUserUseCase } from './DeleteUserUseCase';
+
+class DeleteUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+    const deleteUserUseCase = new DeleteUserUseCase();
+    await deleteUserUseCase.execute(id);
+
+    return response.status(204).json({ message: 'User deleted' });
+  }
+}
+
+export { DeleteUserController };
