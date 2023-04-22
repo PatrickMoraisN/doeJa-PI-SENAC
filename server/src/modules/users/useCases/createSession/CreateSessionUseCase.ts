735fe4a0d67ec6cb09de6ef42b524762c869eb90
Patrick Morais <ppternunes@gmail.com>
2023-04-22 16:01:50 -0300

    feat(CreateSession): create UseCase

---
+++ server/src/modules/users/useCases/createSession/CreateSessionUseCase.ts
@@ -0,0 +1,49 @@
+import { User } from '@modules/users/infra/typeorm/entities/Users';
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { getCustomRepository } from 'typeorm';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+class CreateSessionUseCase {
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const token = sign(
+      {
+        uuid: user.id,
+      },
+      authConfig.jwt.secret,
+      {
+        subject: user.id,
+        expiresIn: authConfig.jwt.expiresIn,
+      },
+    );
+
+    return { user, token };
+  }
+}
+
+export { CreateSessionUseCase };
