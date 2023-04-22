ecea5cc862b56b3d656ba3eb23fe3c97298cdbe1
Patrick Morais <ppternunes@gmail.com>
2023-04-22 15:57:22 -0300

    feat(Users): create Entity

---
+++ server/src/modules/users/infra/typeorm/entities/Users.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  Entity,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  isADonor: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
