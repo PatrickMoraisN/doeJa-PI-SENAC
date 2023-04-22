9157db480ab0ff430543f0195e1e79f20f6ae512
Patrick Morais <ppternunes@gmail.com>
2023-04-22 15:59:59 -0300

    feat(DeleteUser): create UseCase

---
+++ server/src/modules/users/useCases/deleteUser/DeleteUserUseCase.ts
@@ -0,0 +1,19 @@
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+class DeleteUserUseCase {
+  async execute(id: string): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findById(id);
+    console.log('aaaa' + id);
+    console.log('bbbb' + user);
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    await usersRepository.remove(user);
+  }
+}
+
+export { DeleteUserUseCase };
