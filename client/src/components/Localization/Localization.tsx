2930a992e9872977a6d4999e55fe12aaaa1cec13
Patrick Morais <ppternunes@gmail.com>
2023-04-25 10:38:55 -0300

    feat(Localization): create component

---
+++ client/src/components/Localization/Localization.tsx
@@ -0,0 +1,88 @@
+// @ts-ignore
+import { calcCrow } from "@/api/shipping";
+import { sede } from "@/enums/sede";
+import React, { useState } from "react";
+import * as S from "./Localization.styles";
+
+type LocalizationErrorProps = string | null;
+
+type CoordsProps = {
+  latitude: number;
+  longitude: number;
+};
+
+type PositionCoordsProps = {
+  coords: CoordsProps;
+};
+
+type LocalizationProps = {
+  setShipping: (value: number) => void;
+  setShippingCalculated: (value: boolean) => void;
+  error: string | null;
+  methodSelected: string | null;
+};
+
+export function Localization({
+  setShipping,
+  setShippingCalculated,
+  error,
+  methodSelected,
+}: LocalizationProps) {
+  const [coords, setCoords] = useState<CoordsProps | any>(null);
+  const [localizationError, setLocalizationError] =
+    useState<LocalizationErrorProps>(null);
+
+  async function getLocation() {
+    try {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        setLocalizationError("Geolocation is not supported by this browser.");
+      }
+    } catch (err) {
+      setLocalizationError("Error.");
+    }
+  }
+
+  function showPosition(position: PositionCoordsProps) {
+    setCoords({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+
+  function calculateShip() {
+    const { latitude, longitude } = sede;
+    const { latitude: l2, longitude: lo2 } = coords;
+    const distance = calcCrow(latitude, longitude, Number(l2), Number(lo2));
+    return distance;
+  }
+
+  function getShipping() {
+    const shipping = calculateShip() * 2;
+    setShipping(shipping);
+    setShippingCalculated(true);
+    return shipping.toFixed(2);
+  }
+
+  function shippingError() {
+    return error === "O Frete não está calculado";
+  }
+
+  return (
+    <>
+      {!coords && !localizationError && methodSelected === "delivery" && (
+        <S.Button onClick={getLocation} error={shippingError()}>
+          Calcular Frete
+        </S.Button>
+      )}
+      {localizationError && <p>Erro ao calcular!</p>}
+      {coords && !localizationError && (
+        <>
+          <p>Entrega</p>
+          <p>R$ {getShipping()}</p>
+        </>
+      )}
+    </>
+  );
+}
